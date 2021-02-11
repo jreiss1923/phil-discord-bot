7611b28f121bbf116afc354ffac13f9f6cce2922
jreiss1923 <joshua.reiss2@gmail.com>
2021-02-10 23:15:59 -0500

    Added some pseudocode to remember what I'm doing

--- phil-bot.py
+++ phil-bot.py
@@ -1,4 +1,5 @@
 import discord
+import twitter
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -7,6 +8,11 @@ load_dotenv()
 class AutoPhilled(discord.Client):
     async def on_ready(self):
         print('Here and ready to ping Phil!')
+    #async def twitter update
+    #   if twitter api gets followers>prev_followers and followers%5==0:
+    #       send message "@everyone congratulate phil on more followers"
+    #   if twitter api gets phil tweet
+    #       send message "@everyone 'link to tweet'"
 
 
 client = AutoPhilled()
